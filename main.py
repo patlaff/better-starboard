3f0d3a029d9c57996773814283ee4cac0589818e
Patrick Lafferty <patlaff728@gmail.com>
2022-08-22 23:52:13 -0400

    have working edit functionality

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ import datetime
 
 ### GLOBAL  VARS ###
 sb_channel_name = "starboard-testing"
-reaction_count_threshold = 2
+reaction_count_threshold = 3
 
 ### CONSTRUCTORS ###
 load_dotenv()
 client = discord.Client()
 conn = sql.connect('sb.db')
+
+### GLOBAL FUNCTIONS ###
+def createEmbed(message, payload, reaction):
+    # Create Embed Content
+    embedVar = discord.Embed(description=message.content, color=0xffffff)
+    embedVar.set_author(name=message.author.name, icon_url=message.author.avatar_url_as(size=1024))
+    embedVar.insert_field_at(index=1, name="Message", value=f"[Link]({message.jump_url})", inline=True)
+    embedVar.insert_field_at(index=2, name="Channel", value=message.channel.name, inline=True)
+    embedVar.insert_field_at(index=3, name="Reaction", value=f"{payload.emoji}({reaction.count})")
+    return embedVar
 
 @client.event
 async def on_ready():
@@ -20,29 +30,61 @@ async def on_ready():
 @client.event
 async def on_raw_reaction_add(payload):
     print(f'{payload.member.name} added the reaction, {payload.emoji} to the message with ID: {payload.message_id} in channel, {client.get_channel(payload.channel_id)}')
-    guild = client.get_guild(payload.guild_id)
-    channel = guild.get_channel(payload.channel_id)
-    message = await channel.fetch_message(payload.message_id)
+
+    # Open DB Cursor
+    cur = conn.cursor()
+
+    # Commit some common vars
+    guild_id = payload.guild_id
+    channel_id = payload.channel_id
+    message_id = payload.message_id
+
+    # Get context objects
+    guild = client.get_guild(guild_id)
+    channel = guild.get_channel(channel_id)
+    message = await channel.fetch_message(message_id)
 
     # Get Starboard Channel
     sb_channel = discord.utils.get(guild.channels, name=sb_channel_name)
     sb_channel_id = sb_channel.id
 
-    # Get all starred messages in Starboard Channel
-    # sb_messages = [message async for message in sb_channel.history(limit=200) b]
-    # sb_message_list = [id for message in sb_messages]
+    # Get all starred messages in Starboard Channel from DB
+    cur.row_factory = lambda cursor, row: row[0]
+    message_ids = cur.execute("SELECT message_id FROM PINS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchall()
+    print(f"Starred Messages: {message_ids}")
 
     for reaction in message.reactions:
 
-        if reaction.count > reaction_count_threshold:
+        if message_id in message_ids:
+            print('Message already starred. Updating reaction count...')
+
+            # Update starred message with new reaction count
+            if reaction.count >= reaction_count_threshold:
+                starred_id = cur.execute("SELECT sb_message_id FROM PINS WHERE message_id=:message_id", {"message_id": message_id}).fetchone()
+                starred_message = await sb_channel.fetch_message(starred_id)
+
+                embedVar = createEmbed(message, payload, reaction)
+                await starred_message.edit(embed=embedVar)
+
+            return
+
+        if reaction.count >= reaction_count_threshold:
             print(f'Messaged qualifies for starboard. Posting to starboard channel, {sb_channel_name}...')
             channel = client.get_channel(sb_channel_id)
-            # Create Embed Content
-            embedVar = discord.Embed(description=message.content, color=0xffffff)
-            embedVar.set_author(name=message.author.name, icon_url=message.author.avatar_url_as(size=1024))
-            embedVar.insert_field_at(index=1, name="Message", value=f"[Link]({message.jump_url})", inline=True)
-            embedVar.insert_field_at(index=2, name="Channel", value=message.channel.name, inline=True)
-            embedVar.insert_field_at(index=3, name="Reaction", value=f"{payload.emoji}({reaction.count})")
+
+            embedVar = createEmbed(message, payload, reaction)
             starred_message = await channel.send(embed=embedVar)
+
+            cur.execute(f"""
+                INSERT INTO PINS VALUES (
+                    '{starred_message.id}',
+                    '{guild_id}',
+                    '{channel_id}',
+                    '{message_id}'
+                )
+            """)
+            conn.commit()
+    
+    cur.close()
         
 client.run(os.getenv('BS_TOKEN'))
--- sql.py
+++ sql.py
@@ -4,12 +4,14 @@ conn = sql.connect('sb.db')
 
 ## SQL DB SETUP ###
 # with conn:
+#     conn.execute("DROP TABLE PINS;")
+#
+# with conn:
 #     conn.execute("""
 #         CREATE TABLE PINS (
-#             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+#             sb_message_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 #             guild_id INTEGER NOT NULL,
 #             channel_id INTEGER NOT NULL,
-#             message_id INTEGER NOT NULL,
-#             sb_message_id INTEGER NOT NULL
+#             message_id INTEGER NOT NULL
 #         );
 #     """)
