249261d72c51c4404845bbce2f647ad3a1e698db
Pat Lafferty <patlaff728@gmail.com>
2022-09-03 17:17:11 -0400

    added conditional for embed created in status request

--- better-starboard.py
+++ better-starboard.py
@@ -227,22 +227,26 @@ async def status(ctx):
         await ctx.channel.send(response)
         logger.info(response)
         return
-    try:
-        sb_channel_name = cur.execute("SELECT sb_channel_name FROM CONFIGS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchone()
-        reaction_count_threshold = cur.execute("SELECT reaction_count_threshold FROM CONFIGS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchone()
-        channel_exceptions = cur.execute("SELECT channel_name FROM CHANNEL_EXCEPTIONS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchall()
-        reaction_exceptions = cur.execute("SELECT reaction FROM REACTION_EXCEPTIONS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchall()
 
-        # Create Embed Content
-        embedVar = discord.Embed(title=f"{guild.name} {bot_name} configuration:", color=0xffffff)
-        embedVar.insert_field_at(index=1, name="Better-Starboard Channel", value=sb_channel_name, inline=True)
-        embedVar.insert_field_at(index=2, name="Reaction Count Threshold", value=reaction_count_threshold, inline=True)
-        embedVar.insert_field_at(index=3, name="Channel Exceptions", value='\u200'.join((f"- {i}\n" for i in channel_exceptions)), inline=False)
-        embedVar.insert_field_at(index=4, name="Reaction Exceptions", value='\u200'.join((f"- {i}\n" for i in reaction_exceptions)), inline=True)
-        await ctx.channel.send(embed=embedVar)
+    sb_channel_name = cur.execute("SELECT sb_channel_name FROM CONFIGS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchone()
+    reaction_count_threshold = cur.execute("SELECT reaction_count_threshold FROM CONFIGS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchone()
+    channel_exceptions = cur.execute("SELECT channel_name FROM CHANNEL_EXCEPTIONS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchall()
+    reaction_exceptions = cur.execute("SELECT reaction FROM REACTION_EXCEPTIONS WHERE guild_id=:guild_id", {"guild_id": guild_id}).fetchall()
+
+    # Create Embed Content
+    embedVar = discord.Embed(title=f"{guild.name} {bot_name} configuration:", color=0xffffff)
+    embedVar.insert_field_at(index=1, name="Better-Starboard Channel", value=sb_channel_name, inline=True)
+    embedVar.insert_field_at(index=2, name="Reaction Count Threshold", value=reaction_count_threshold, inline=True)
+    if len(channel_exceptions)==0:
+        embedVar.insert_field_at(index=3, name="Channel Exceptions", value='None', inline=False)
+    else:
+        embedVar.insert_field_at(index=3, name="Channel Exceptions", value=''.join((f"- {i}\n" for i in channel_exceptions)), inline=False)
+    if len(reaction_exceptions)==0:
+        embedVar.insert_field_at(index=4, name="Reaction Exceptions", value='None', inline=True)
+    else:
+        embedVar.insert_field_at(index=4, name="Reaction Exceptions", value=''.join((f"- {i}\n" for i in reaction_exceptions)), inline=True)
     
-    except Exception as e:
-        await ctx.channel.send(e)
+    await ctx.channel.send(embed=embedVar)
 
     cur.close()
 
